e41022175438910568bc9da59e72c78b85b6b05f
isabelcristinapereira <isabelcristina214@outlook.com>
2021-09-28 22:33:30 -0300

    cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { MenuComponent } from './menu/menu.component';
+
 
 
 
 @NgModule({
   declarations: [
-    AppComponent 
+    AppComponent,
+    MenuComponent,
+    CadastroComponent 
   ],
   imports: [
     BrowserModule
